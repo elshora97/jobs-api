a3b47211b495c0eaf38e076747179e8849d5e3ff
elshora97 <A.elshora@dataserve.com.sa>
2024-03-06 13:33:11 +0200

    Added get single job method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
-const { UnauthenticatedError, BadRequestError } = require("../errors/index");
+const { UnauthenticatedError, NotFoundError } = require("../errors/index");
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
@@ -8,8 +8,20 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`There is no job with ID: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
